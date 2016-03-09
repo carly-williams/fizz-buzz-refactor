bd90256e6746e5211f054cbed1050b81e3fc982f
Carly Williams <carly.kurth@gmail.com>
2016-03-09 00:21:47 -0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	
+	$('.input-number').focus();
+
+	$('.input-number').keydown(function(event) {
+		if (event.keyCode === 13) {
+			var num = $('.input-number').val();
+			$('.output').empty();
+			fnCountAndConvertUpToNum(num);
+		}
+	});
+
+	function fnCountAndConvertUpToNum(num) {
+
+		if (num % 1 != 0) {
+			$('.output').append("Please enter an integer.");
+		}
+		else {
+			var count = 1;
+
+			while (count <= num) {
+
+				if ((count%3 == 0) && (count%5 == 0)) {
+					$('.output').append("FizzBuzz" + "<br>");
+				}
+				else if (count%3 == 0) {
+					$('.output').append("Fizz" + "<br>");
+				}
+				else if (count%5 == 0) {
+					$('.output').append("Buzz" + "<br>");
+				}
+				else {
+					$('.output').append(count + "<br>");
+				}
+				count++;
+			}					
+		}
+	}
+});
